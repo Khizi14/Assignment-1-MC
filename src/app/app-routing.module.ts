f161763b7c31255e2695b9fa2805f738e6fa7fe4
Khizi14 <khizi72@gmail.com>
2022-10-22 18:27:07 +0500

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'goal',
+    loadChildren: () => import('./pages/goal/goal.module').then( m => m.GoalPageModule)
+  },
+  {
+    path: 'personas',
+    loadChildren: () => import('./pages/personas/personas.module').then( m => m.PersonasPageModule)
+  },
+  {
+    path: 'sketches',
+    loadChildren: () => import('./pages/sketches/sketches.module').then( m => m.SketchesPageModule)
+  },
+  {
+    path: 'critique',
+    loadChildren: () => import('./pages/critique/critique.module').then( m => m.CritiquePageModule)
+  },
+  {
+    path: 'tecnologies',
+    loadChildren: () => import('./pages/tecnologies/tecnologies.module').then( m => m.TecnologiesPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
